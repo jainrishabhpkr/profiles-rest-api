9f4d3fafa1f4b60993142c8a4a510956a16b16db
rishabh jain <jainrishabhpkr@gmail.com>
2019-05-08 15:33:25 +0530

    added viewset and serializers

--- src/profileproject/profiles_api/views.py
+++ src/profileproject/profiles_api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status, viewsets
+
+from . import serializers
 # Create your views here.
 
 
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
     def get(self,request,format=None):
 
         an_apiview=['apple',
@@ -12,4 +17,31 @@ class HelloApiView(APIView):
                       'cat',
                       'dog']
         return Response({'message':'hello','an_apiview':an_apiview})
-        
+
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method':'put andn '})
+
+
+    def patch(self, request, pk=None):
+        return Response({'method':'patch'})
+
+
+    def delete(self,request, pk=None):
+        return Response({'message':'delete'})
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+    def list(self,request):
+        a_viewset=['chotu','tanu','tony','pony','sonu','monu']
+        return Response({'message':'hello','a_viewset':a_viewset})
