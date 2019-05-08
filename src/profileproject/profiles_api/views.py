a18720801ba1a5a7b93dc58075299dcedf347e5e
rishabh jain <jainrishabhpkr@gmail.com>
2019-05-08 17:58:42 +0530

    added login api

--- src/profileproject/profiles_api/views.py
+++ src/profileproject/profiles_api/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets , filters
 from rest_framework.authentication import TokenAuthentication
+
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 from . import models
 from . import permissions
 
@@ -84,3 +87,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
 
     search_fields  = ('name','email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        return ObtainAuthToken().post(request)
