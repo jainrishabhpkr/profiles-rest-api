914474650840f46e21a47b19ea8eb834ee7bef0a
rishabh jain <jainrishabhpkr@gmail.com>
2019-05-08 15:47:04 +0530

    added viewset methods

--- src/profileproject/profiles_api/views.py
+++ src/profileproject/profiles_api/views.py
@@ -42,6 +42,30 @@ class HelloApiView(APIView):
 
 
 class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+
     def list(self,request):
         a_viewset=['chotu','tanu','tony','pony','sonu','monu']
         return Response({'message':'hello','a_viewset':a_viewset})
+
+    def create(self,request):
+        serializer = serializers.HelloSerializer(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def retreive(self, request, pk=None):
+        return Response({"http_method":"GET"})
+
+    def update(self, request, pk=None):
+        return Response({"http_method":"PUT"})
+
+    def partial_update(self, request, pk=None):
+        return Response({"http_method":"PATCH"})
+
+    def destroy(self, request, pk=None):
+        return Response({"http_method":"DELETE"})
