6cccf42ada9488d2c5ee0a5d0596195be0cb0707
rishabh jain <jainrishabhpkr@gmail.com>
2019-05-08 17:39:44 +0530

    search added

--- src/profileproject/profiles_api/views.py
+++ src/profileproject/profiles_api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets , filters
 from rest_framework.authentication import TokenAuthentication
 from . import models
 from . import permissions
@@ -81,3 +81,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+
+    search_fields  = ('name','email',)
